830906a47564eada288a65ae590262c83614439f
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-03 15:44:47 -0300

    Publish app source

--- app/src/main/java/com/example/data/AppInfo.kt
+++ app/src/main/java/com/example/data/AppInfo.kt
@@ -1,10 +1,12 @@
 package com.example.data
 
 import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.ImageBitmap
 
 data class AppInfo(
     val packageName: String,
     val appName: String,
     val icon: Drawable,
-    val className: String
+    val className: String,
+    val iconBitmap: ImageBitmap? = null
 )
